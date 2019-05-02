6edaad4c82834b3424a0e71740e9ee44fcbdb48f
Adam Birr <abirr@hotmail.com>
2019-05-02 21:56:13 +0100

    results

--- test100k.sh
+++ test100k.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-words=`cat 1k.txt`
-words100k=''
-
-for i in {1..100}
-do
-	words100k=$words100k$words
-done
 
 echo -----------------
 echo save start time: $(date +"%T.%3N")
 curl -H "Content-Type: application/x-www-form-urlencoded" \
 	--request POST \
-	--data "$words100k" \
-         https://deepmatter.appspot.com/save?filename=test100ka
+	--data @100k.txt \
+         https://deepmatter.appspot.com/save?filename=test100kb1
 echo save end time: $(date +"%T.%3N")
 
 echo -----------------
 echo retrieve start time: $(date +"%T.%3N")
 curl -H "Content-Type: text/plain" \
 	--request GET \
-         https://deepmatter.appspot.com/retrieve?filename=test100ka
+         https://deepmatter.appspot.com/retrieve?filename=test100kb1
 echo retrieve end time: $(date +"%T.%3N")
 echo -----------------
